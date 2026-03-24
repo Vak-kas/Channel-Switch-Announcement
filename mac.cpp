1cf5d5fbe06b5cbe19362a7768b305aa9affe288
서민재 <hey_minj@naver.com>
2026-03-24 13:43:56 +0900

    feat: mac입력 문자열 처리

--- mac.cpp
+++ mac.cpp
@@ -1,8 +1,25 @@
 #include "mac.h"
 #include "pch.h"
 
-Mac::operator std::string() const {
+Mac::operator std::string() const 
+{
     char buffer[20];
     snprintf(buffer, sizeof(buffer), "%02X:%02X:%02X:%02X:%02X:%02X", mac_[0], mac_[1], mac_[2], mac_[3], mac_[4], mac_[5]);
     return std::string(buffer);
+}
+
+Mac::Mac(const std::string& mac_str) 
+{
+    if(sscanf(mac_str.c_str(), "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &mac_[0], &mac_[1], &mac_[2], &mac_[3], &mac_[4], &mac_[5]) != 6) 
+    {
+        throw std::invalid_argument("Invalid MAC address format");
+    }
+}
+
+Mac::Mac(const char* mac_str) 
+{
+    if(sscanf(mac_str, "%hhx:%hhx:%hhx:%hhx:%hhx:%hhx", &mac_[0], &mac_[1], &mac_[2], &mac_[3], &mac_[4], &mac_[5]) != 6) 
+    {
+        throw std::invalid_argument("Invalid MAC address format");
+    }
 }
--- mac.h
+++ mac.h
@@ -13,9 +13,11 @@ private:
     uint8_t mac_[SIZE];
 
 public:
-    Mac();
+    Mac() : mac_{0} {};
     Mac(const uint8_t* other) {memcpy(this->mac_, other, SIZE);}
     Mac(const Mac& other) { memcpy(this->mac_, other.mac_, SIZE); }
+    Mac(const std::string& mac_str);
+    Mac(const char* mac_str);
 
     explicit operator std::string() const;
 
