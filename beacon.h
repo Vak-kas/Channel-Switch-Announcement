68f617e106c886cc2717120362f8c6f393e47028
서민재 <hey_minj@naver.com>
2026-03-24 17:00:21 +0900

    fix : 줄바꿈 이쁘게

--- beacon.h
+++ beacon.h
@@ -1,8 +1,10 @@
+#pragma once
 #include <stdlib.h>
+#include <cstdint>
 struct beacon_frame_body
 {
     uint64_t timestamp;
-    uint16_t beacon_interval; //default : 100ms
+    uint16_t beacon_interval; //default : 100ms (초당 10회 비콘 프레임 발송)
     uint16_t capability_info;
     char* ssid;
 };
