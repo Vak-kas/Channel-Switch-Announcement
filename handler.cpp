3e7c81e112245e3abfa3f2c4b2dc1f5d5528c259
서민재 <hey_minj@naver.com>
2026-03-24 17:04:53 +0900

    feat: 원하는 타킷 비콘 프레임 찾는 핸들러 완성

--- handler.cpp
+++ handler.cpp
@@ -1,5 +1,7 @@
 #include "handler.h"
 #include "pch.h"
+#include "radiotap.h"
+#include "macframe.h"
 
 
 pcap_t* handle_init(const char* dev, char* errbuf) {
@@ -22,5 +24,47 @@ pcap_t* handle_init(const char* dev, char* errbuf) {
 
     std::cout << "DLT OK: " << dlt << std::endl;
     return pcap;
+}
 
+void dump(const u_char* buf, int size) {
+    int i;
+    for (i = 0; i < size; i++) {
+        if (i != 0 && i % 16 == 0)
+            printf("\n");
+        printf("%02X ", buf[i]);
+    }
+    printf("\n");
+}
+
+u_char* find_target_beacon(pcap_t* pcap, Mac ap_mac, int* len) {
+    while (true) 
+    {
+        struct pcap_pkthdr* header;
+        const u_char* packet;
+        int res = pcap_next_ex(pcap, &header, &packet);
+        if (res == 0)
+        {
+            continue; //time out
+        } 
+        else if(res <0)
+        { // error
+            printf("pcap_next_ex return %d(%s)\n", res, pcap_geterr(pcap));
+            break;
+        }
+
+        ieee80211_radiotap_header* radiotap_header = (ieee80211_radiotap_header*) packet;
+        // dump(packet, header->caplen);
+        const u_char* ieee80211_header = packet + radiotap_header->it_len;
+        ieee80211_mac_header* mac_header = (ieee80211_mac_header*) ieee80211_header;
+
+        if(isBeaconFrame(mac_header) && isTargetAP(mac_header, ap_mac)) 
+        {
+            std::cout << "Found target AP's beacon frame!" << std::endl;
+            *len = header->caplen;
+            return (u_char*)ieee80211_header;
+        }
+
+    }
+    //TODO : 특정 시간 동안 못찾으면 종료하는 로직 추가해야함.
+    return nullptr;
 }
--- handler.h
+++ handler.h
@@ -1,4 +1,7 @@
 #pragma once
 #include <pcap.h>
+#include "mac.h"
 
 pcap_t* handle_init(const char* dev, char* errbuf);
+u_char* find_target_beacon(pcap_t* pcap, Mac ap_mac, int* len);
+void dump(const u_char* buf, int size);
