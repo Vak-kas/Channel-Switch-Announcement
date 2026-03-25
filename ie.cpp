33362eaacc0b4932a025a1778e20f0fa60e0f337
서민재 <hey_minj@naver.com>
2026-03-25 16:20:09 +0900

    feat : CSA 구조체 및 생성 코드

--- ie.cpp
+++ ie.cpp
@@ -1,1 +1,12 @@
 #include "ie.h"
+
+channel_switch_announcement_ie create_channel_switch_announcement_ie(uint8_t new_channel_number, uint8_t channel_switch_count) 
+{
+    channel_switch_announcement_ie ie;
+    ie.element_id = ieee80211_IE::CHANNEL_SWITCH_ANNOUNCEMENT;
+    ie.length = 3; // channel_switch_mode(1) + new_channel_number(1) + channel_switch_count(1)
+    ie.channel_switch_mode = 1; // 1 : STA should stop transmission until switch, 0: STA can transmit until switch
+    ie.new_channel_number = new_channel_number;
+    ie.channel_switch_count = channel_switch_count;
+    return ie;
+}
--- ie.h
+++ ie.h
@@ -1,1 +1,29 @@
 #pragma once
+#include <stdlib.h>
+
+struct channel_switch_announcement_ie
+{
+    uint8_t element_id;
+    uint8_t length;
+    uint8_t channel_switch_mode;
+    uint8_t new_channel_number;
+    uint8_t channel_switch_count;
+} __attribute__((__packed__));
+
+
+channel_switch_announcement_ie create_channel_switch_announcement_ie(uint8_t new_channel_number, uint8_t channel_switch_count);
+
+enum ieee80211_IE 
+{
+    SSID = 0,
+    SUPPORTED_RATES = 1,
+    DSSS_PARAMETER_SET = 3,
+    TIM = 5,
+    COUNTRY = 7,
+    POWER_CONSTRAINT = 32,
+    SUPPORTED_CHANNELS = 36,
+    CHANNEL_SWITCH_ANNOUNCEMENT = 37,
+    QUIET  = 40,
+    VENDOR_SPECIFIC = 221
+}; 
+//Reference : https://www.nsnam.org/docs/release/3.28/doxygen/wifi-information-element_8h_source.html
