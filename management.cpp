d3f3646dba78a5e3a3748d5432eba27130b23fbf
서민재 <hey_minj@naver.com>
2026-03-26 16:26:51 +0900

    feat: 비콘 프레임 정보 출력 코드 생성 및 beacon_frame_body 패딩 출력 버그 수정

--- management.cpp
+++ management.cpp
@@ -1,1 +1,9 @@
 #include "management.h"
+#include "pch.h"
+
+void print_beacon_frame_body(const beacon_frame_body* body)
+{
+    std::cout << "Timestamp: " << body->timestamp << std::endl;
+    std::cout << "Beacon Interval: " << body->beacon_interval << std::endl;
+    std::cout << "Capability Info: " << std::hex << body->capability_info << std::dec << std::endl;
+}
--- management.h
+++ management.h
@@ -6,4 +6,6 @@ struct beacon_frame_body
     uint64_t timestamp;
     uint16_t beacon_interval; //default : 100ms (초당 10회 비콘 프레임 발송)
     uint16_t capability_info;
-};
+} __attribute__((__packed__));
+
+void print_beacon_frame_body(const beacon_frame_body* body);
